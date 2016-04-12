119c43ad11572b39c10a0f521e706d53ca36bb45
Amit_Kaspi <akaspi@wix.com>
2016-04-12 17:10:32 +0300

    use http server on production and express on development

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
 /* eslint no-undef: "off", no-console: "off" */
 
+var config = require('./config.json');
 var express = require('express');
 var path = require('path');
-var webpack = require('webpack');
-var config = require('./config.json');
 var app = express();
+var server = require('http').createServer(app);
+
+server.listen(process.env.PORT || config.prodPort, function () {
+    console.log('Production server listening at port %d', server.address().port);
+});
+
+var static_path = path.join(path.resolve(path.dirname()), config.publicFolder);
+app.use(express.static(static_path));
 
 var isDevelopment = (process.env.NODE_ENV !== 'production');
-var static_path = path.join(path.resolve(path.dirname()), config.publicFolder);
-
-var prodListener = app.use(express.static(static_path))
-    .get('/', function (req, res) {
-        res.sendFile('index.html', {
-            root: static_path
-        });
-    }).listen(process.env.PORT || config.prodPort, function (err) {
-        if (err) { console.log(err); }
-        console.log('Production is listening at localhost:' + prodListener.address().port);
-    });
-
 if (isDevelopment) {
-    var webpackDevConf = require('./webpack.config');
+    var webpackDevConf = require('./webpack.config.js');
+    var webpack = require('webpack');
     var WebpackDevServer = require('webpack-dev-server');
 
-    new WebpackDevServer(webpack(webpackDevConf), {
+    var devServer = new WebpackDevServer(webpack(webpackDevConf), {
         publicPath: webpackDevConf.output.publicPath,
         hot: true,
         stats: { colors: true }
-    }).listen(config.devPort, 'localhost', function (err) {
-        if (err) { console.log(err); }
-        console.log('Development is listening at localhost:' + config.devPort);
     });
+
+    devServer.listen(config.devPort, function (err) {
+        if (err) { console.log(err); }
+        console.log('Development server listening at port %d', config.devPort);
+    });
+
 }
